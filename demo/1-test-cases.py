651b4c5978576006623c1a164b0d9d514212cd1e
Michael Harrell <183651098+csaea@users.noreply.github.com>
2026-02-27 17:39:20 +0000

    adds in-class demo files

---
+++ demo/1-test-cases.py
@@ -0,0 +1,29 @@
+import sys
+import math
+import string
+
+# Read number of test cases from stdin.
+Test_line = sys.stdin.readline().rstrip() # default is a STRING
+
+T = int(Test_line)
+print("Number of test cases:", T)
+
+for i in range(T):
+    # Read the next line from standard input.
+    # Each test case is assumed to contain two integers separated by whitespace.
+    # eg. 3 5
+
+    # Split the line into separate string tokens.
+
+    # Convert string tokens into integers using map().
+    # map() is lazy, meaning conversion happens during iteration.
+
+    # Compute the required outputs.
+    # compute the total of both numbers added together
+    # compute the product of both numbers
+
+    # print result
+
+
+
+
